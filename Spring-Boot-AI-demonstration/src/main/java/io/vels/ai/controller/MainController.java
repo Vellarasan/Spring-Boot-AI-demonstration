44a7a12f3a61d22d6810eacc99fd890b885c60d9
vellarasan <vellarasan21@gmail.com>
2025-02-16 20:06:52 -0600

    - Enhanced the project to with Prompt templates and some output parser. - Included String Template files for prompts.

--- Spring-Boot-AI-demonstration/src/main/java/io/vels/ai/controller/MainController.java
+++ Spring-Boot-AI-demonstration/src/main/java/io/vels/ai/controller/MainController.java
@@ -4,31 +4,53 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1")
 public class MainController {
 
     Logger log = LoggerFactory.getLogger(MainController.class);
 
     private final ChatClient chatClient;
 
+    @Value("classpath:/prompts/simple-prompts.st")
+    private Resource simplePromptsResource;
+
     public MainController(ChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
-    @GetMapping("/chat")
-    public String chat() {
-        log.debug("Invoking the Chat AI...");
-        Prompt prompt = new Prompt("what is an AI?");
-        ChatResponse chatResponse = chatClient.prompt(prompt)
-                .call()
-                .chatResponse();
-        return chatResponse != null ? chatResponse
-                .getResult()
-                .getOutput()
-                .getContent() : "Unable to Complete the Chat! Please try again";
+    @GetMapping("/chatResponseAsMap")
+    public Map<String, Object> chatResponseAsMap(@RequestParam(value = "utility-name", defaultValue = "financial") String utilityName) {
+
+
+        log.info("Constructing Prompt...");
+
+        MapOutputConverter mapOutputConverter = new MapOutputConverter();
+        String format = mapOutputConverter.getFormat();
+        Prompt prompt = new PromptTemplate(simplePromptsResource)
+                .create(Map.of("utility-name", utilityName, "format", format));
+
+        log.info("Invoking the OpenAI...");
+        ChatResponse chatResponse = chatClient.prompt(prompt).call().chatResponse();
+
+        if (chatResponse != null) {
+            Generation generation = chatResponse.getResult();
+            return mapOutputConverter.convert(generation.getOutput().getContent());
+        }
+        return Collections.emptyMap();
     }
 }
