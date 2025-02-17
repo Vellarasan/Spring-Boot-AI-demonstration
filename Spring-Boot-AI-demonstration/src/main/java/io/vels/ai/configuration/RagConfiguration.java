d2766dc9c2f6331429c9b1df6ba1cb9cb2034779
vellarasan <vellarasan21@gmail.com>
2025-02-16 22:48:19 -0600

    - Enhanced the Project with RAG (Retrieval-augmented generation) with SimpleVectorStore. - Minor refactors

---
+++ Spring-Boot-AI-demonstration/src/main/java/io/vels/ai/configuration/RagConfiguration.java
@@ -0,0 +1,70 @@
+package io.vels.ai.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Configuration
+public class RagConfiguration {
+
+    Logger log = LoggerFactory.getLogger(RagConfiguration.class);
+
+    @Value("classpath:/docs/spring-ai-upgrade-notes.txt")
+    private Resource springAiReleaseNotes;
+
+    @Value("${vectorstore.filename}")
+    private String vectorStoreFileName;
+
+    @Bean
+    SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel) {
+        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
+
+        // Get the doc for embedding...
+        File vectorStoreFile = getVectorStoreFile();
+
+        if (vectorStoreFile.exists()) {
+            log.info("Vector Store File already exist, loading it now...");
+            simpleVectorStore.load(vectorStoreFile);
+        } else {
+            log.info("Vector Store File not found, loading the document to vector store...");
+
+            // Read the text file
+            TextReader textReader = new TextReader(springAiReleaseNotes);
+
+            // Setting basic meta data
+            textReader.getCustomMetadata().put("filename", "spring-ai-release-notes.txt");
+            List<Document> documents = textReader.get();
+
+            // Splitting the documents
+            log.info("Splitting the documents...");
+            TokenTextSplitter tokenTextSplitter = new TokenTextSplitter();
+            List<Document> splitDocs = tokenTextSplitter.split(documents);
+
+            // Adding the Split Documents to the Simple Vector Store
+            log.info("Adding the split documents to the vector store");
+            simpleVectorStore.add(splitDocs);
+            simpleVectorStore.save(vectorStoreFile);
+        }
+        return simpleVectorStore;
+    }
+
+    private File getVectorStoreFile() {
+        Path path = Paths.get("Spring-Boot-AI-demonstration", "src", "main", "resources", "data");
+        String absolutePath = path.toFile().getAbsolutePath() + "/" + vectorStoreFileName;
+        return new File(absolutePath);
+    }
+
+}
--- Spring-Boot-AI-demonstration/src/main/java/io/vels/ai/controller/MainController.java
+++ Spring-Boot-AI-demonstration/src/main/java/io/vels/ai/controller/MainController.java
@@ -8,6 +8,9 @@ import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,12 +29,17 @@ public class MainController {
     Logger log = LoggerFactory.getLogger(MainController.class);
 
     private final ChatClient chatClient;
+    private final VectorStore vectorStore;
 
     @Value("classpath:/prompts/simple-prompts.st")
-    private Resource simplePromptsResource;
+    private Resource simplePromptTemplate;
 
-    public MainController(ChatClient chatClient) {
+    @Value("classpath:/prompts/rag-prompt.st")
+    private Resource ragPromptTemplate;
+
+    public MainController(ChatClient chatClient, VectorStore vectorStore) {
         this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/chatResponseAsMap")
@@ -41,16 +50,46 @@ public class MainController {
 
         MapOutputConverter mapOutputConverter = new MapOutputConverter();
         String format = mapOutputConverter.getFormat();
-        Prompt prompt = new PromptTemplate(simplePromptsResource)
+        Prompt prompt = new PromptTemplate(simplePromptTemplate)
                 .create(Map.of("utility-name", utilityName, "format", format));
 
-        log.info("Invoking the OpenAI...");
-        ChatResponse chatResponse = chatClient.prompt(prompt).call().chatResponse();
+        ChatResponse chatResponse = invokeOpenAI(prompt);
 
         if (chatResponse != null) {
             Generation generation = chatResponse.getResult();
             return mapOutputConverter.convert(generation.getOutput().getContent());
         }
         return Collections.emptyMap();
+    }
+
+
+    @GetMapping("chatUsingRag")
+    public String chatUsingRag(@RequestParam(value = "q", defaultValue = "What is the latest release note version?") String question) {
+
+        // Get the similar vectors using the simple vector store
+        List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.builder().query(question).topK(2).build());
+
+        if (similarDocuments == null) {
+            return "No similarities identified. Please try again with different input";
+        }
+        List<String> documentsList = similarDocuments.stream().map(Document::getText).toList();
+
+        // Constructing the Prompts
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+
+        Prompt prompt = promptTemplate.create(Map.of("input", question, "documents", String.join("\n", documentsList)));
+
+        ChatResponse chatResponse = invokeOpenAI(prompt);
+
+        if (chatResponse != null) {
+            return chatResponse.getResult().getOutput().getContent();
+        }
+
+        return "No response from AI. Please try again!";
+    }
+
+    private ChatResponse invokeOpenAI(Prompt prompt) {
+        log.info("Invoking the OpenAI...");
+        return chatClient.prompt(prompt).call().chatResponse();
     }
 }
